400675ac0cd0d41efe81b74f42ded1302d2c2dbf
bunny <banhibikashsaha@gmail.com>
2026-02-02 20:45:57 +0530

    created a program which returns a virtual file type

---
+++ Controllers/File Results/Controllers/FileController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace File_Results.Controllers
+{
+    public class FileController : Controller
+    {
+        //using virtual file result whe the file is in wwwroot folder
+        [Route("/")]
+        public VirtualFileResult Index()
+        {
+            return new VirtualFileResult("/doc.pdf","application/pdf");
+        }
+    }
+}
