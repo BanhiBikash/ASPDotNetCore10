fe0a74835c5b177688baa1fe8aa5c98f9a2bf35c
bunny <banhibikashsaha@gmail.com>
2026-02-05 12:45:57 +0530

    Created a program to take input through Model binding via Form Data and Form URL Encoded

---
+++ Model Binding/Form URLEncoded and Form Data/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Form_URLEncoded_and_Form_Data.Models;
+
+namespace Form_URLEncoded_and_Form_Data.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return Content("Welcome to the Home Controller of form data!", "text/html");
+        }
+
+        //receives student type parameters
+        [Route("/students/{Name?}/{Id:int?}/{City?}")]
+        public IActionResult Data(Students students)
+        {
+            return Content($"\n-----------\nStudent Name:{students.Name}  Id:{students.Id}  City:{students.City}\n---------\n ", "text/html");
+        }
+    }
+}
---
+++ Model Binding/Form URLEncoded and Form Data/Models/Students.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Form_URLEncoded_and_Form_Data.Models
+{
+    public class Students
+    {
+        //this takes value in accordnace to precedence
+        public string? Name { get; set; }
+
+        //this takes value in accordnace to precedence
+        public int? Id { get; set; }
+
+        //this takes value in accordnace to precedence
+        public string? City { get; set; }
+    }
+}
