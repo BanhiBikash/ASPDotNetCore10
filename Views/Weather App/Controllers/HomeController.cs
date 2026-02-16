53016f7e0c0bbe167431a6668fd961380b28d6a9
bunny <banhibikashsaha@gmail.com>
2026-02-16 20:29:43 +0530

    Created a weather app which shows weather data of multiple cities or a single city when a specific city is selected through sending that city code, used Models, Views,Controllers and Routing

---
+++ Views/Weather App/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using Weather_App.Models;
+
+namespace Weather_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<City> cities = new List<City>()
+        {
+                     
+            new City{
+                CityUniqueCode = "LDN",
+                CityName = "London",
+                DateAndTime = Convert.ToDateTime("2030-01-01 08:00"),
+                TemperatureFahrenheit = 33
+            },
+          
+            new City{
+                CityUniqueCode = "NYC",
+                CityName = "New York",
+                DateAndTime = Convert.ToDateTime("2030-01-01 03:00"),
+                TemperatureFahrenheit = 60
+            },
+           
+            new City{
+                CityUniqueCode = "PAR",
+                CityName = "Paris",
+                DateAndTime = Convert.ToDateTime("2030-01-01 09:00"),
+                TemperatureFahrenheit = 82
+            }
+
+        };
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(cities);
+        }
+
+        [Route("/weather/{cityCode:required}")]
+        public IActionResult SpecificCity()
+        {
+            City? data = cities.FirstOrDefault(c =>
+           c.CityUniqueCode.Equals(Convert.ToString(Request.RouteValues["cityCode"]), StringComparison.OrdinalIgnoreCase));
+
+            if (data != null)
+            {
+                return View(data);
+            }
+            else
+            {
+                return NotFound("City not found.");
+            }
+        }
+    }
+}
