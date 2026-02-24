4cd1dcf905190ff430dd3c887a97edb8b738c1f6
bunny <banhibikashsaha@gmail.com>
2026-02-24 16:23:58 +0530

    created a program which saves config data in app settings and accesses it through proram.cs files or via controller thorugh IConfiguration interface

---
+++ Configurations/ConfigAppSettings/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConfigAppSettings.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewBag.ConfigData = _configuration.GetValue<string>("MyKey");
+            return View();
+        }
+    }
+}
---
+++ Configurations/ConfigAppSettings/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+app.UseStaticFiles();   
+app.MapControllers();
+
+string response = string.Empty;
+
+app.Map(("/config"), async(HttpContext context) =>
+{
+    response = app.Configuration["MyKey"] + "\n";
+    response += app.Configuration.GetValue<string>("MyKey") + "\n";
+    response = response + app.Configuration.GetValue<int>("Digit", 7).ToString();
+    await context.Response.WriteAsync(response);
+});
+
+app.Run();
