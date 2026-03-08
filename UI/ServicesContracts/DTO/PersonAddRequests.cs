568251ef6a5cf4fd7ea96a11ff8b4a58705f7520
bunny <banhibikashsaha@gmail.com>
2026-03-08 12:30:57 +0530

    Added Validations

--- UI/ServicesContracts/DTO/PersonAddRequests.cs
+++ UI/ServicesContracts/DTO/PersonAddRequests.cs
@@ -1,20 +1,27 @@
 ﻿using Entities;
 using ServicesContracts.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace ServicesContracts.DTO
 {
     public class PersonAddRequests
     {
+        [Required(ErrorMessage = "PersonName is required.")]
         public string? PersonName { get; set; }
 
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
         public string? Email { get; set; }
 
+        [Required(ErrorMessage = "DateOfBirth is required.")]
         public DateTime? DateOfBirth { get; set; }
 
+        [Required]
         public GenderValues? Gender { get; set; }
 
+        [Required]
         public string? Address { get; set; }
 
+        [Required(ErrorMessage = "CountryID is required")]
         public string? CountryID { get; set; }
 
         public Person ConvertToPerson()
