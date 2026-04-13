888a35dbbbf5326729bc0b2c680b2c392a7772bc
bunny <banhibikashsaha@gmail.com>
2026-04-13 21:15:24 +0530

    Created Identity Entities and Inherited IdentityDbContext

--- CleanArchitecture/ContactsManager.Infrastructure/DBContext/ApplicationDbContext.cs
+++ CleanArchitecture/ContactsManager.Infrastructure/DBContext/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Data.SqlClient;
 using ContactsManager.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using ContactsManager.Core.Domain.IdentityEntities;
 
 namespace ContactsManager.Infrastructure.DBContext
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser,ApplicationRole,Guid>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
