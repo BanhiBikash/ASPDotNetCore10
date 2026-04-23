0d534109a1cd049c82579c6bf685ba468954af6d
bunny <banhibikashsaha@gmail.com>
2026-04-23 16:47:18 +0530

    Successfully added swagger api versioning in Assignment

--- WebAPISolution/OrderAPI.Web/Program.cs
+++ WebAPISolution/OrderAPI.Web/Program.cs
@@ -23,15 +23,30 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetValue<string>("ConnectionStrings:DefaultConnection"));
 });
+
 builder.Services.AddEndpointsApiExplorer(); //reads all the action methods/endpoints
+
 builder.Services.AddSwaggerGen(options =>
 {
-
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo() {Title="Orders Version 1", Version="1.0"});
+    options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "Orders Version 1", Version = "2.0" });
 });     //create the open api code according to that
+
+//swagger is told that how versions are substituted
+builder.Services.AddApiVersioning().AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+
+///////
 var app = builder.Build();
 
 app.UseSwagger();   //creates endpoint for swagger.json
-app.UseSwaggerUI();     //creates the swagger UI
+app.UseSwaggerUI(options =>{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json","1.0");
+    options.SwaggerEndpoint("/swagger/v2/swagger.json","2.0");
+});     //creates the swagger UI
 
 // Configure the HTTP request pipeline.
 app.UseHsts();
