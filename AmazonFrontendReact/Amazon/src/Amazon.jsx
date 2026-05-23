e1e6b4a529f94fdaed997baaf887fcb23c55206e
bunny <banhibikashsaha@gmail.com>
2026-05-23 21:06:01 +0530

    Created UserContext and UserContextProvider

--- AmazonFrontendReact/Amazon/src/Amazon.jsx
+++ AmazonFrontendReact/Amazon/src/Amazon.jsx
@@ -2,20 +2,23 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer"
 import { Outlet } from "react-router-dom";
+import UserContextProvider from "./context/UserContextProvider";
 
-export const Amazon=()=>{
-    return(
-    <div className="app-layout-wrapper">
-      {/* Locked to viewport top */}
-      <Navbar />
-      
-      {/* Dynamic page context insertion area */}
-      <main className="main-content-fluid">
-        <Outlet />
-      </main>
-      
-      {/* Base alignment ground layer */}
-      <Footer />
-    </div>
+export const Amazon = () => {
+    return (
+        <UserContextProvider>
+            <div className="app-layout-wrapper">
+                {/* Locked to viewport top */}
+                <Navbar />
+
+                {/* Dynamic page context insertion area */}
+                <main className="main-content-fluid">
+                    <Outlet />
+                </main>
+
+                {/* Base alignment ground layer */}
+                <Footer />
+            </div>
+        </UserContextProvider>
     )
 }
