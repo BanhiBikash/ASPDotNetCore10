a17c85e58716fd6453cf4fdae986e3f7df4e8f96
bunny <banhibikashsaha@gmail.com>
2026-06-12 16:53:01 +0530

    Integrated Topic addition according to category in Product rows

--- AmazonFrontendReact/Amazon/src/Components/ProductRow.jsx
+++ AmazonFrontendReact/Amazon/src/Components/ProductRow.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom';
 
 const ProductRow = (props) => {
 
+    // const [catName, setCatName] = useState("")
     const row = props.row;
+    const catName = row[0]?.category || "";
+
     const navigate = useNavigate();
 
   return (
     
       <div className="wide-deals-strip-container">
-        <h2 className="strip-section-title">Today's Deals | Handpicked Top Offers</h2>
+        <h2 className="strip-section-title">Today's {catName || ""} Deals | Handpicked Top Offers</h2>
         <div className="horizontal-scroll-row">
           {Array.isArray(row) && row.map(item => {
             return (<div className="deal-thumb-box">
